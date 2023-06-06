adcfef9c21013f08d8e00e26c356f4bad4d44654
osayenko0315 <126353984+osayenko0315@users.noreply.github.com>
2023-06-05 19:23:33 -0500

    java

---
+++ assets/question.js
@@ -0,0 +1,79 @@
+let questions = [
+    {
+        numb: 1,
+        question: "JavaScript is which type of language?",
+        answer: "Object-Based",
+        options: [
+            "Object-Oriented",
+            "Object-Based",
+            "Assembly-language",
+            "High-level"
+        ]
+    },
+    {
+        numb: 2,
+        question: "When interpreter encounters an empty statement, what will it do?",
+        answer: "Ignore the statement",
+        options: [
+            "Show a warning",
+            "Prompt to complete the statement",
+            "Throw an error",
+            "Ignore the statement",
+        ]
+    },
+    {
+        numb: 3,
+        question: "JavaScript is interpreted by _________",
+        answer: "Client",
+        options: [
+            "Client",
+            "Server",
+            "Object",
+            "None of the above",
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which of the following is not a valid JavaScript variable name?",
+        answer: "2names",
+        options: [
+            "2names",
+            "_first_and_last_names",
+            "FirstAndLast",
+            "None of the above",
+        ]
+    },
+    {
+        numb: 5,
+        question : "The _______ method of an Array object adds and/or removes elements from an array.",
+        answer: "Slice",
+        options: [
+            "Reverse",
+            "Shift",
+            "Slice",
+            "Splice",
+        ]
+    },
+    {
+        numb: 6,
+        question : "Using _______ statement is how you test for a specific condition.",
+        answer: "If",
+        options: [
+            "Select",
+            "If",
+            "Switch",
+            "For",
+        ]
+    },
+    {
+        numb: 7,
+        question : "Which of the following is not considered a JavaScript operator?",
+        answer: "delete",
+        options: [
+            "new",
+            "this",
+            "delete",
+            "typeof",
+        ]
+    },
+];
