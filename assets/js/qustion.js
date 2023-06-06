d66ffd708e91ac4ba890836bb8b8ec9071129a9c
osayenko0315 <126353984+osayenko0315@users.noreply.github.com>
2023-06-05 20:21:03 -0500

    qustions

---
+++ assets/js/qustion.js
@@ -0,0 +1,53 @@
+let questions = [
+    {
+        question: "JavaScript is which type of language?",
+        answer: "Object-Based",
+        choices: [
+            "Object-Oriented",
+            "Object-Based",
+            "Assembly-language",
+            "High-level"
+        ]
+    },
+    {
+
+        question: "What html tag is NOT included in the HEAD tag?",
+        answer: "header",
+        options: [
+            "link",
+            "meta",
+            "title",
+            "header"  ]   
+
+    },
+    {
+        question: "JavaScript is interpreted by",
+        answer: "Client",
+        choices: [
+            "Client",
+            "Server",
+            "Object",
+            "None of the above",
+        ]
+    },
+    {
+        question: "Which of the following is not a valid JavaScript variable name?",
+        answer: "2names",
+        choices: [
+            "2names",
+            "_first_and_last_names",
+            "FirstAndLast",
+            "None of the above",
+        ]
+    },
+    {
+        question: "Which CSS property controls text size?",
+        answer: "font-size",
+        choices: [
+            "font-size",
+            "text-size",
+            "font-style",
+            "size"
+        ]
+    },
+]
